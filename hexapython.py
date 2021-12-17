fd1e7a1486b43cdbc776f50ea4ac9e46c192d1a5
master23456789 <91898768+master23456789@users.noreply.github.com>
2021-12-17 20:01:23 +0530

    Add files via upload

---
+++ hexapython.py
@@ -0,0 +1,17 @@
+#WAP in python there you take 2 inputs from users and check if the entered number is equal to 16 then you print that either the 1st or the second number entered by the useer is hexaphone number.
+
+a = int(input('The first number by the user:'))
+b = int(input('The second number by the user:'))
+if a == 16:
+    print('The first no. is equal to 16.')
+elif b == 16:
+    print('The second no. is equal to 16.')
+    
+
+
+
+
+
+
+
+
---
+++ palpython.py
@@ -0,0 +1,7 @@
+#WAP in python to take one input from the user and check if the no. is equal to 21 print that it is not a palindrome no. else it is a palindrone number.
+
+p = int(input('Enter the number'))
+if p == 21:
+    print('It is not a palindrone number.')
+else:
+    print('It is a palindrone number.')
